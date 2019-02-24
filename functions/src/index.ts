607d103b8b3599ea61d3ef2b777466bebd932aaa
yggilabs <skelley.android@gmail.com>
2019-02-24 13:18:45 -0600

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,21 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as express from 'express';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as express from 'express'
 
-const app = express();
+const app = express()
 
-admin.initializeApp(); // this automatically handles all the configuration
+admin.initializeApp() // this automatically handles all the configuration
 
 app.get('/test/', async (request, response) => { // async arrow functions allows you to use 'await' int he function body
   try {
-    response.send({status: 'ok', message: 'hello world'}); // all endpoints need to call res.send()
+    response.send({
+      status: 'ok',
+      message: 'hello world'
+    }) // all endpoints need to call res.send()
   } catch(error) { // if something bad happens
-    console.log(error); // log the error
-    response.status(500).send(error); // send a server error status code
+    console.log(error) // log the error
+    response.status(500).send(error) // send a server error status code
   }
-});
+})
 
-exports.api = functions.https.onRequest(app); // run the app at the 'api' endpoint
+exports.api = functions.https.onRequest(app) // run the app at the 'api' endpoint
